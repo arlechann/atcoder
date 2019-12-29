a02491a5bce508c7ef0451fb4ab7b3941a7c2dd9
arlechann <dragnov3728@gmail.com>
2019-12-29 22:20:07 +0900

    ABC149

---
+++ AtCoder/ABC149/C.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) (a).begin(), (a).end()
+#define AALL(a, n) (a), ((a) + (n))
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define MODNUM (static_cast<int>(1e9 + 7))
+#define MOD(x) ((x) % MODNUM)
+
+using namespace std;
+
+using ll = long long;
+using VI = vector<int>;
+using VI2D = vector<vector<int>>;
+
+const int INF = 2e9;
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, -1, 0, 1};
+
+template <typename T>
+int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T>
+T square(T x) {
+	return x * x;
+}
+
+inline int toInt(string s) {
+	int v;
+	istringstream sin(s);
+	sin >> v;
+	return v;
+}
+
+constexpr bool is_prime(int n) {
+	if(n == 2) {
+		return true;
+	}
+
+	if(n % 2 == 0) {
+		return false;
+	}
+
+	for(int i = 3; i * i <= n; i += 2) {
+		if(n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int x;
+	scanf("%d", &x);
+	int i;
+	for(i = x; !is_prime(i); i++)
+		;
+	printf("%d\n", i);
+	return 0;
+}
---
+++ AtCoder/ABC149/D.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) (a).begin(), (a).end()
+#define AALL(a, n) (a), ((a) + (n))
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define MODNUM (static_cast<int>(1e9 + 7))
+#define MOD(x) ((x) % MODNUM)
+
+using namespace std;
+
+using ll = long long;
+using VI = vector<int>;
+using VI2D = vector<vector<int>>;
+
+const int INF = 2e9;
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, -1, 0, 1};
+
+template <typename T>
+int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T>
+T square(T x) {
+	return x * x;
+}
+
+inline int toInt(string s) {
+	int v;
+	istringstream sin(s);
+	sin >> v;
+	return v;
+}
+
+int r, s, p;
+int win(char c) {
+	switch(c) {
+		case 'r':
+			return p;
+		case 's':
+			return r;
+		case 'p':
+			return s;
+	}
+}
+
+int main() {
+	int n, k;
+
+	scanf("%d %d", &n, &k);
+	scanf("%d %d %d", &r, &s, &p);
+	char t[n + 1];
+	scanf("%s", t);
+	vector<vector<char>> rsp(k);
+	REP(i, n) { rsp[i % k].push_back(t[i]); }
+
+	int result = 0;
+	REP(i, k) {
+		int size = rsp[i].size();
+		VI dp(size, 0);
+		REP(j, size) {
+			if(j == 0 || rsp[i][j] != rsp[i][j - 1]) {
+				dp[j] += (j == 0 ? 0 : dp[j - 1]) + win(rsp[i][j]);
+			} else {
+				dp[j] = dp[j - 1];
+				rsp[i][j] = '-';
+			}
+		}
+		result += dp[size - 1];
+	}
+	printf("%d\n", result);
+	return 0;
+}
