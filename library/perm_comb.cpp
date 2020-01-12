ac391a10723bd7e88d51a63efc07f639146fab41
arlechann <dragnov3728@gmail.com>
2020-01-12 23:12:55 +0900

    delete 'using namespace std' and macros

--- library/perm_comb.cpp
+++ library/perm_comb.cpp
@@ -3,47 +3,35 @@
 #include <iostream>
 #include <vector>
 
-#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < (i##_MACRO); i++)
-#define RANGE(i, a, b) for(int i = (a); i < (b); i++)
-#define ALL(a) (a).begin(), (a).end()
-
-using namespace std;
-
-using ll = long long;
-using VI = vector<int>;
-using VI2D = vector<vector<int>>;
-
 // permutation ここから
 // 順列
-ll permutation(int n, int k) {
+long long permutation(int n, int k) {
 	if(n < k || n < 0 || k < 0) {
 		return 0LL;
 	}
 
 	ll ret = 1;
-	REP(i, k) { ret *= n - i; }
+	for(int i = 0; i < k; i++) {
+		ret *= n - i;
+	}
 	return ret;
 }
 // premutation ここまで
 
 // ALL_PERM ここから
 // 引数の関数にすべての順列を渡して実行する 実行した関数がtrueを返すと打ち切る
-#define ALL_PERM(v, lambda)                     \
+#define ALL_PERM(v, block)                      \
 	call_with_all_permutations(v, [&](auto v) { \
-		lambda;                                 \
+		block;                                  \
 		return false;                           \
 	})
 template <typename T, typename F>
 void call_with_all_permutations(T a, F proc) {
-#ifndef ALL
-	static_assert(false, "ALL() is not defined.");
-#endif
-
 	do {
 		if(proc(a)) {
 			return;
 		};
-	} while(next_permutation(ALL(a)));
+	} while(std::next_permutation(a.begin(), a.end()));
 }
 // ALL_PREM ここまで
 
@@ -51,21 +39,19 @@ void call_with_all_permutations(T a, F proc) {
 template <typename T>
 T gcd(T a, T b) {
 	if(a < b) {
-		swap(a, b);
+		std::swap(a, b);
 	}
-
 	T r = a % b;
 	while(r != 0) {
 		a = b;
 		b = r;
 		r = a % b;
 	}
-
 	return b;
 }
 
 // 二項係数
-ll combination(int n, int r) {
+long long combination(int n, int r) {
 	if(n < r || n < 0 || r < 0) {
 		return 0LL;
 	}
@@ -76,8 +62,8 @@ ll combination(int n, int r) {
 		r = n - r;
 	}
 
-	ll ret = 1;
-	RANGE(i, 1, r + 1) {
+	long long ret = 1;
+	for(int i = 1; i < r + 1; i++) {
 		ret *= (n - i + 1);
 		ret /= i;
 	}
@@ -86,8 +72,8 @@ ll combination(int n, int r) {
 // combination ここまで
 
 int main() {
-	VI v = {1, 2, 3, 4, 5};
-	VI2D a;
+	std::vector<int> v = {1, 2, 3, 4, 5};
+	std::vector<vector<int>> a;
 
 	ALL_PERM(v, {
 		a.push_back(v);
@@ -96,8 +82,8 @@ int main() {
 		}
 	});
 
-	for_each(a.begin(), a.end(), [](VI x) {
-		for_each(x.begin(), x.end(), [](int n) { cout << n << " "; });
+	std::for_each(a.begin(), a.end(), [](VI x) {
+		std::for_each(x.begin(), x.end(), [](int n) { cout << n << " "; });
 		cout << endl;
 	});
 
