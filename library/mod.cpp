e50ec5099d290561d759e926a7c6c812dd6e8062
arlechann <dragnov3728@gmail.com>
2020-01-12 23:11:26 +0900

    add mod_inv()

--- library/mod.cpp
+++ library/mod.cpp
@@ -1,59 +1,47 @@
 #include <vector>
 
-#define RANGE(i, a, b) for(int i = (a), i < (b), i++)
-#define MODNUM (1e9 + 7)
-#define MOD(x) ((x) % static_cast<int>(MODNUM))
-
-using namespace std;
-using ll = long long;
-
 // mod_pow ここから
-ll mod_pow(ll a, ll n) {
-#ifndef MOD
-	static_assert(false, "MOD() is not defined.");
-#endif
+long long mod_pow(long long a, long long n, long long mod) {
 	ll ret = 1;
 	while(n != 0) {
 		if(n % 2) {
-			ret = MOD(ret * a);
+			ret = (ret * a) % mod;
 		}
-		a = MOD(a * a);
-		n /= 2;
+		a = (a * a) % mod, n /= 2;
 	}
 	return ret;
 }
 // mod_pow ここまで
 
+// modの逆元
+long long mod_inv(long long n, long long mod) {
+	return mod_pow(a, mod - 2, mod);
+}
+
 // mod_comb ここから
 // modを取りつつ二項係数を計算する関数を返す
-auto make_mod_comb() {
-#ifndef MODNUM
-	static_assert(false, "MODNUM is not defined.");
-#endif
-#ifndef MOD
-	static_assert(false, "MOD() is not defined.");
-#endif
+auto make_mod_comb(long long mod) {
 	const int COMB_MAX = 10000000;
-	vector<ll> fact(COMB_MAX);
-	vector<ll> fact_inv(COMB_MAX);
-	vector<ll> inv(COMB_MAX);
+	vector<long long> fact(COMB_MAX);
+	vector<long long> fact_inv(COMB_MAX);
+	vector<long long> inv(COMB_MAX);
 
 	fact[0] = fact[1] = 1;
 	fact_inv[0] = fact_inv[1] = 1;
 	inv[1] = 1;
 
-	RANGE(i, 2, COMB_MAX) {
-		fact[i] = MOD(fact[i - 1] * i);
-		inv[i] = MODNUM - MOD(inv[MODNUM % i] * (MODNUM / i));
-		fact_inv[i] = MOD(fact_inv[i - 1] * inv[i]);
+	for(int i = 2; i < COMB_MAX; i++) {
+		fact[i] = (fact[i - 1] * i) % mod;
+		inv[i] = mod - (inv[mod % i] * (mod / i)) % mod;
+		fact_inv[i] = (fact_inv[i - 1] * inv[i]) % mod;
 	}
 
-	return
-		[fact = move(fact), fact_inv = move(fact_inv)](const ll n, const ll r) {
-			if(n < r || n < 0 || r < 0) {
-				return 0LL;
-			}
-			return MOD(fact[n] * MOD(fact_inv[r] * fact_inv[n - r]));
-		};
+	return [mod = mod, fact = move(fact), fact_inv = move(fact_inv)](
+			   const long long n, const long long r) {
+		if(n < r || n < 0 || r < 0) {
+			return 0LL;
+		}
+		return (fact[n] * ((fact_inv[r] * fact_inv[n - r]) % mod)) % mod;
+	};
 }
 // mod_comb ここまで
