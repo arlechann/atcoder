08a9f4e31a098fbda734937e539219e9dc47225f
arlechann <dragnov3728@gmail.com>
2020-01-19 01:31:28 +0900

    delete "using namespace std"

--- library/gcd_lcm.cpp
+++ library/gcd_lcm.cpp
@@ -1,14 +1,10 @@
 #include <functional>
 
-using namespace std;
-
-// lcm ここから
-// gcd ここから
 // 最大公約数を返す
 template <typename T>
 T gcd(T a, T b) {
 	if(a < b) {
-		swap(a, b);
+		std::swap(a, b);
 	}
 
 	T r = a % b;
@@ -20,11 +16,9 @@ T gcd(T a, T b) {
 
 	return b;
 }
-// gcd ここまで
 
 // 最小公倍数を返す
 template <typename T>
 T lcm(T a, T b) {
 	return a / gcd(a, b) * b;
 }
-// lcm ここまで
