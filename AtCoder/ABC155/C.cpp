fe4f001fc250226f9f70a5504106b50cd631b681
arlechann <dragnov3728@gmail.com>
2020-02-17 00:18:16 +0900

    ABC155

---
+++ AtCoder/ABC155/C.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) (a).begin(), (a).end()
+#define AALL(a, n) (a), ((a) + (n))
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define INT(x) (static_cast<int>(x))
+#define MODNUM (INT(1e9 + 7))
+#define MOD(x) ((x) % MODNUM)
+
+using namespace std;
+
+using ll = long long;
+using VI = vector<int>;
+using VI2D = vector<vector<int>>;
+
+const int INF = 2e9;
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, -1, 0, 1};
+
+template <typename T>
+int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T>
+void chmax(T& m, T x) {
+	m = max(m, x);
+}
+
+template <typename T>
+void chmin(T& m, T x) {
+	m = min(m, x);
+}
+
+template <typename T>
+T square(T x) {
+	return x * x;
+}
+
+inline int toInt(string s) {
+	int v;
+	istringstream sin(s);
+	sin >> v;
+	return v;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<string> si(n);
+	REP(i, n) { cin >> si[i]; }
+	vector<string> result;
+	map<string, int> count;
+	REP(i, n) { count[si[i]]++; }
+	int max = 0;
+	EACH(e, count) { chmax(max, e.second); }
+	EACH(e, count) {
+		if(e.second == max) {
+			cout << e.first << endl;
+		}
+	}
+	return 0;
+}
---
+++ AtCoder/ABC155/E.cpp
@@ -0,0 +1,122 @@
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) (a).begin(), (a).end()
+#define AALL(a, n) (a), ((a) + (n))
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define INT(x) (static_cast<int>(x))
+#define MODNUM (INT(1e9 + 7))
+#define MOD(x) ((x) % MODNUM)
+
+using namespace std;
+
+using ll = long long;
+using VI = vector<int>;
+using VI2D = vector<vector<int>>;
+
+const int INF = 2e9;
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, -1, 0, 1};
+
+template <typename T>
+int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T>
+void chmax(T& m, T x) {
+	m = max(m, x);
+}
+
+template <typename T>
+void chmin(T& m, T x) {
+	m = min(m, x);
+}
+
+template <typename T>
+T square(T x) {
+	return x * x;
+}
+
+inline int toInt(string s) {
+	int v;
+	istringstream sin(s);
+	sin >> v;
+	return v;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	int len = s.length();
+	VI n(len + 1, 0);
+	REP(i, len) { n[i] = s[len - (i + 1)] - '0'; }
+	int result = 0;
+	bool carry = false;
+	REP(i, len + 1) {
+		if(carry) {
+			if(n[i] + 1 == 5 && i != len) {
+				if(n[i + 1] >= 5) {
+					result += 10 - n[i] - 1;
+					carry = true;
+				} else {
+					result += n[i] + 1;
+					carry = false;
+				}
+			} else if(n[i] + 1 >= 5) {
+				result += 10 - (n[i] + 1);
+				carry = true;
+			} else {
+				result += (n[i] + 1);
+				carry = false;
+			}
+		} else {
+			if(n[i] == 5 && i != len) {
+				if(n[i + 1] >= 5) {
+					result += 10 - n[i];
+					carry = true;
+				} else {
+					result += n[i];
+					carry = false;
+				}
+			} else if(n[i] >= 5) {
+				result += 10 - n[i];
+				carry = true;
+			} else {
+				result += n[i];
+				carry = false;
+			}
+		}
+	}
+
+	cout << result << endl;
+	return 0;
+}
