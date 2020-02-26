17158ed63e9961fc4ed9bc3c4a27b26b2c526d3e
arlechann <dragnov3728@gmail.com>
2020-02-26 16:46:36 +0900

    use constexpr

--- library/mod.cpp
+++ library/mod.cpp
@@ -1,113 +1,118 @@
+#include <array>
 #include <vector>
 
 // 計算量 前処理:O(n) クエリ:O(1)
 template <long long MOD = 1000000007>
-ModInt<MOD> mod_comb(long long n, long long r) {
-	const int COMB_MAX = 1000000;
-	static vector<ModInt<MOD>> fact(COMB_MAX);
-	static vector<ModInt<MOD>> fact_inv(COMB_MAX);
-	static vector<ModInt<MOD>> inv(COMB_MAX);
+class mod_comb {
+	constexpr int COMB_MAX = 1000000;
+	std::array<ModInt<MOD>, COMB_MAX> fact;
+	std::array<ModInt<MOD>, COMB_MAX> fact_inv;
+	std::array<ModInt<MOD>, COMB_MAX> inv;
 
-	if(n < r || n < 0 || r < 0) {
-		return ModInt<MOD>(0);
-	}
+	public:
+	constexpr mod_comb() : fact{}, fact_inv{}, inv{} {}
 
-	if(fact[0] == 0) {
-		fact[0] = fact[1] = 1;
-		fact_inv[0] = fact_inv[1] = 1;
-		inv[1] = 1;
+	constexpr int init() {
+		this->fact[0] = this->fact[1] = 1;
+		this->fact_inv[0] = this->fact_inv[1] = 1;
+		this->inv[1] = 1;
 
 		for(int i = 2; i < COMB_MAX; i++) {
-			fact[i] = fact[i - 1] * i;
-			inv[i] = MOD - (inv[MOD % i] * (MOD / i));
-			fact_inv[i] = fact_inv[i - 1] * inv[i];
+			this->fact[i] = this->fact[i - 1] * i;
+			this->inv[i] = MOD - (this->inv[MOD % i] * (MOD / i));
+			this->fact_inv[i] = this->fact_inv[i - 1] * this->inv[i];
 		}
+		return 0;
 	}
 
-	return fact[n] * fact_inv[r] * fact_inv[n - r];
-}
+	constexpr ModInt<MOD> operator()(long long n, long long r){
+		if(n < r || n < 0 || r < 0) {
+			return ModInt<MOD>(0);
+		}
+
+		if(this->fact[0] == 0){
+			this->init();
+		}
+		return fact[n] * fact_inv[r] * fact_inv[n - r];
+	}
+};
 
 template <long long MOD = 1000000007>
 class ModInt {
 	public:
 	long long n;
 
-	ModInt() : n(0) {}
-	ModInt(long long n) : n(n) {
-		while(this->n < 0) {
-			this->n += MOD;
-		}
-		this->n %= MOD;
-	}
+	constexpr ModInt() : n(0) {}
+	constexpr ModInt(long long n) : n(n < 0 ? n + MOD : n % MOD) {}
 
-	long long get() const { return this->n; }
-	long long get_mod() const { return MOD; }
+	constexpr long long get() const { return this->n; }
+	constexpr long long get_mod() const { return MOD; }
 
-	ModInt inv() const { return pow<ModInt<>>(*this, MOD - 2); }
+	constexpr ModInt inv() const { return pow<ModInt<MOD>>(*this, MOD - 2); }
 
-	ModInt& operator=(const long long rhs) { return *this = ModInt(rhs); }
-	ModInt& operator+=(const ModInt rhs) {
+	constexpr ModInt& operator=(const long long rhs) { return *this = ModInt(rhs); }
+	constexpr ModInt& operator+=(const ModInt rhs) {
 		return *this = ModInt(this->n + rhs.n);
 	}
-	ModInt& operator-=(const ModInt rhs) {
+	constexpr ModInt& operator-=(const ModInt rhs) {
 		return *this = ModInt(this->n - rhs.n);
 	}
-	ModInt& operator*=(const ModInt rhs) {
+	constexpr ModInt& operator*=(const ModInt rhs) {
 		return *this = ModInt(this->n * rhs.n);
 	}
-	ModInt& operator/=(const ModInt rhs) { return *this *= rhs.inv(); }
-	bool operator==(const ModInt rhs) const { return this->n == rhs.n; }
+	constexpr ModInt& operator/=(const ModInt rhs) { return *this *= rhs.inv(); }
+	constexpr bool operator==(const ModInt rhs) const { return this->n == rhs.n; }
 };
 
 template <long long MOD>
-ModInt<MOD> operator+(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) += rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator+(const ModInt<MOD>& lhs, const long long& rhs) {
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs, const long long& rhs) {
 	return ModInt<MOD>(lhs) += rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator+(const long long& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator+(const long long& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) += rhs;
 }
 
 template <long long MOD>
-ModInt<MOD> operator-(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) -= rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator-(const ModInt<MOD>& lhs, const long long& rhs) {
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs, const long long& rhs) {
 	return ModInt<MOD>(lhs) -= rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator-(const long long& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator-(const long long& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) -= rhs;
 }
 
 template <long long MOD>
-ModInt<MOD> operator*(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator*(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) *= rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator*(const ModInt<MOD>& lhs, const long long& rhs) {
+constexpr ModInt<MOD> operator*(const ModInt<MOD>& lhs, const long long& rhs) {
 	return ModInt<MOD>(lhs) *= rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator*(const long long& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator*(const long long& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) *= rhs;
 }
 
 template <long long MOD>
-ModInt<MOD> operator/(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator/(const ModInt<MOD>& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) /= rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator/(const ModInt<MOD>& lhs, const long long& rhs) {
+constexpr ModInt<MOD> operator/(const ModInt<MOD>& lhs, const long long& rhs) {
 	return ModInt<MOD>(lhs) /= rhs;
 }
 template <long long MOD>
-ModInt<MOD> operator/(const long long& lhs, const ModInt<MOD>& rhs) {
+constexpr ModInt<MOD> operator/(const long long& lhs, const ModInt<MOD>& rhs) {
 	return ModInt<MOD>(lhs) /= rhs;
 }
 
--- library/template.cpp
+++ library/template.cpp
@@ -24,8 +24,8 @@
 #define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
 #define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
 #define EACH(e, a) for(auto&& e : a)
-#define ALL(a) (a).begin(), (a).end()
-#define AALL(a, n) (a), ((a) + (n))
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
 #define FILL(a, n) memset((a), n, sizeof(a))
 #define FILLZ(a) FILL(a, 0)
 #define INT(x) (static_cast<int>(x))
@@ -36,12 +36,12 @@ using ll = long long;
 using VI = vector<int>;
 using VI2D = vector<vector<int>>;
 
-const int INF = 2e9;
-const double EPS = 1e-10;
-const double PI = acos(-1.0);
+constexpr int INF = 2e9;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
 
-const int dx[] = {-1, 0, 1, 0};
-const int dy[] = {0, -1, 0, 1};
+constexpr int dx[] = {-1, 0, 1, 0};
+constexpr int dy[] = {0, -1, 0, 1};
 
 template <typename T>
 constexpr int sign(T x) {
@@ -53,26 +53,19 @@ constexpr int sign(double x) {
 	return x < -EPS ? -1 : x > EPS ? 1 : 0;
 }
 
-template <typename T>
-void chmax(T& m, T x) {
+template <typename T, typename U>
+constexpr void chmax(T& m, U x) {
 	m = max(m, x);
 }
 
-template <typename T>
-void chmin(T& m, T x) {
+template <typename T, typename U>
+constexpr void chmin(T& m, U x) {
 	m = min(m, x);
 }
 
 template <typename T>
 constexpr T square(T x) {
 	return x * x;
-}
-
-constexpr int toInt(string s) {
-	int v;
-	istringstream sin(s);
-	sin >> v;
-	return v;
 }
 
 int main() {
