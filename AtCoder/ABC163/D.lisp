1335021858fe82fdcef577d98b92ddbf7ed4ef29
arlechann <dragnov3728@gmail.com>
2020-04-20 01:06:49 +0900

    ABC163 D問題AC

--- AtCoder/ABC163/D.lisp
+++ AtCoder/ABC163/D.lisp
@@ -1,3 +1,5 @@
+(defvar mod-number 1000000007)
+ 
 (defun main ())
  
 (defun iota (c &optional (s 0) (step 1) (acc nil))
@@ -5,24 +7,34 @@
       (reverse acc)
       (iota (1- c) (+ s step) step (cons s acc))))
  
-(defun sum (list &optional (acc 0))
-  (if (null list)
-      acc
-      (sum (cdr list) (+ (car list) acc))))
+(defun sum (list &optional (init 0))
+  (reduce #'+ list :initial-value init))
+ 
+(defmacro debug-print (x)
+  `(let ((y ,x))
+    (format t "~A: ~A~%" ',x y)
+	y))
  
 (defparameter n (read))
 (defparameter k (read))
  
-(defparameter a (make-array (1+ n) :initial-contents (iota (1+ n))))
-(defparameter s (make-array (1+ n) :initial-contents (cdr (reverse (reduce (lambda (acc n) (cons (+ n (car acc)) acc)) a :initial-value '(0))))))
+(defparameter s
+  (make-array (+ 2 n)
+    :initial-contents (reverse
+	                    (reduce
+						  (lambda (acc n)
+						    (cons (+ n (car acc))
+							      acc))
+						  (iota (1+ n))
+	                      :initial-value '(0)))))
+ 
+(defun high (pick-num)
+  (- (aref s (1+ n)) (aref s (- (1+ n) pick-num))))
  
 (defun low (pick-num)
   (aref s pick-num))
  
-(defun high (pick-num)
-  (- (aref s n) (aref s (- n pick-num))))
- 
 (defun solve (pick-num)
-  (- (high pick-num) (low pick-num)))
+  (1+ (- (high pick-num) (low pick-num))))
  
-(format t "~A~%" (sum (mapcar #'solve (iota (- (1+ n) k k)))))
+(format t "~A~%" (reduce (lambda (x y) (rem (+ x y) mod-number)) (mapcar #'solve (iota (- (1+ n) (1- k)) k))))
