1bfeec9416f256b8bd78dbb05f04d49c3ec062c3
arlechann <dragnov3728@gmail.com>
2020-07-24 17:34:34 +0900

    Add Counter class

---
+++ AtCoder/AGC031/A2.cpp
@@ -0,0 +1,231 @@
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define INT(x) (static_cast<int>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
+
+using namespace std;
+
+using ll = long long;
+using VI = std::vector<int>;
+using VI2D = std::vector<vector<int>>;
+using VLL = std::vector<long long>;
+using VLL2D = std::vector<vector<long long>>;
+
+constexpr int INF = 2e9;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
+
+constexpr int dx[] = {-1, 0, 1, 0};
+constexpr int dy[] = {0, -1, 0, 1};
+
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T, typename U>
+constexpr void chmax(T& m, U x) {
+	m = max(m, x);
+}
+
+template <typename T, typename U>
+constexpr void chmin(T& m, U x) {
+	m = min(m, x);
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+template <long long MOD = 1000000007>
+class ModInt {
+	public:
+	long long n;
+
+	constexpr ModInt() : n(0) {}
+	constexpr ModInt(long long n) : n(n < 0 ? n + MOD : n % MOD) {}
+
+	constexpr long long get() const { return this->n; }
+	constexpr long long get_mod() const { return MOD; }
+
+	constexpr ModInt inv() const { return pow<ModInt<MOD>>(*this, MOD - 2); }
+
+	constexpr ModInt& operator=(const long long rhs) {
+		return *this = ModInt(rhs);
+	}
+	constexpr ModInt& operator+=(const ModInt rhs) {
+		return *this = ModInt(this->n + rhs.n);
+	}
+	constexpr ModInt& operator-=(const ModInt rhs) {
+		return *this = ModInt(this->n - rhs.n);
+	}
+	constexpr ModInt& operator*=(const ModInt rhs) {
+		return *this = ModInt(this->n * rhs.n);
+	}
+	constexpr ModInt& operator/=(const ModInt rhs) {
+		return *this *= rhs.inv();
+	}
+	constexpr bool operator==(const ModInt rhs) const {
+		return this->n == rhs.n;
+	}
+};
+
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator*(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) *= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator*(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) *= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator*(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) *= rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator/(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) /= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator/(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) /= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator/(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) /= rhs;
+}
+
+template <long long MOD>
+std::ostream& operator<<(std::ostream& os, const ModInt<MOD>& x) {
+	return os << x.n;
+}
+
+template <long long MOD>
+std::istream& operator>>(std::istream& is, const ModInt<MOD>& x) {
+	return is >> x.n;
+}
+
+template <typename T>
+class Counter {
+	using iterator = typename std::unordered_map<T, std::size_t>::iterator;
+	using const_iterator =
+		typename std::unordered_map<T, std::size_t>::const_iterator;
+
+	std::unordered_map<T, std::size_t> m;
+
+	public:
+	template <typename U>
+	Counter(U& v) {
+		for(T& e : v) {
+			this->m[e]++;
+		}
+	}
+
+	std::vector<T> keys() const {
+		std::vector<T> v(this->size());
+		int i = 0;
+		for(const std::pair<T, std::size_t>& e : this->m) {
+			v[i++] = e.first;
+		}
+		return v;
+	}
+
+	std::vector<size_t> values() const {
+		std::vector<size_t> v(this->size());
+		int i = 0;
+		for(const std::pair<T, std::size_t>& e : this->m) {
+			v[i++] = e.second;
+		}
+		return v;
+	}
+
+	std::size_t size() const { return this->m.size(); }
+	iterator begin() { return this->m.begin(); }
+	iterator end() { return this->m.end(); }
+	const_iterator cbegin() const { return this->m.cbegin(); }
+	const_iterator cend() const { return this->m.cend(); }
+
+	const std::size_t& operator[](T a) const& { return this->m[a]; }
+	std::size_t& operator[](T a) & { return this->m[a]; }
+	std::size_t operator[](T a) const&& { return std::move(this->m[a]); }
+};
+
+int main() {
+	int n;
+	string s;
+	cin >> n;
+	cin >> s;
+
+	Counter<char> count(s);
+	ModInt<> result = 1;
+	EACH(e, count.values()) { result *= e + 1; }
+
+	cout << result - 1 << endl;
+	return 0;
+}
--- library/sequence.cpp
+++ library/sequence.cpp
@@ -1,12 +1,47 @@
-#include <map>
 #include <unordered_map>
 #include <vector>
 
 template <typename T>
-std::unordered_map<T, size_t> group_count(std::vector<T>& v) {
-	std::unordered_map<T, size_t> c;
-	for(T& e : v) {
-		c[e]++;
+class Counter {
+	using iterator = typename std::unordered_map<T, std::size_t>::iterator;
+	using const_iterator =
+		typename std::unordered_map<T, std::size_t>::const_iterator;
+
+	std::unordered_map<T, std::size_t> m;
+
+	public:
+	template <typename U>
+	Counter(U& v) {
+		for(T& e : v) {
+			this->m[e]++;
+		}
 	}
-	return c;
-}
+
+	std::vector<T> keys() const {
+		std::vector<T> v(this->size());
+		int i = 0;
+		for(const std::pair<T, std::size_t>& e : this->m) {
+			v[i++] = e.first;
+		}
+		return v;
+	}
+
+	std::vector<size_t> values() const {
+		std::vector<size_t> v(this->size());
+		int i = 0;
+		for(const std::pair<T, std::size_t>& e : this->m) {
+			v[i++] = e.second;
+		}
+		return v;
+	}
+
+	std::size_t size() const { return this->m.size(); }
+	iterator begin() { return this->m.begin(); }
+	iterator end() { return this->m.end(); }
+	const_iterator cbegin() const { return this->m.cbegin(); }
+	const_iterator cend() const { return this->m.cend(); }
+
+	const std::size_t& operator[](T a) const& { return this->m[a]; }
+	std::size_t& operator[](T a) & { return this->m[a]; }
+	std::size_t operator[](T a) const&& { return std::move(this->m[a]); }
+};
