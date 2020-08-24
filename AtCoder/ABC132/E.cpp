e60ce6f960ef7b4330c7262ec27233461edfb70f
arlechann <dragnov3728@gmail.com>
2020-08-24 22:28:59 +0900

    ABC132 E問題 解説AC

--- AtCoder/ABC132/E.cpp
+++ AtCoder/ABC132/E.cpp
@@ -1,80 +1,184 @@
+#include <algorithm>
+#include <boost/optional.hpp>
+#include <climits>
+#include <cmath>
 #include <cstdio>
-#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
 #include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
-#define TIMEUP                   \
-	if(clock() - start > 1800) { \
-		printf("%d\n", -1);      \
-		return 0;                \
-	}
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define INT(x) (static_cast<int>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
 
 using namespace std;
 
-int edge[100001][100001];
+using ll = long long;
+using VI = std::vector<int>;
+using VI2D = std::vector<vector<int>>;
+using VLL = std::vector<long long>;
+using VLL2D = std::vector<vector<long long>>;
 
-int main(void) {
-	int n, m;
-	int u, v;
-	int s, t;
+constexpr int INF = 2e9;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
 
-	clock_t start = clock();
+constexpr int dx[] = {-1, 0, 1, 0};
+constexpr int dy[] = {0, -1, 0, 1};
 
-	scanf("%d %d", &n, &m);
-	for(int i = 0; i < m; i++) {
-		scanf("%d %d", &u, &v);
-		edge[u][v] = 1;
+template <typename T, std::size_t N>
+struct make_vector_type {
+	using type =
+		typename std::vector<typename make_vector_type<T, (N - 1)>::type>;
+};
+
+template <typename T>
+struct make_vector_type<T, 0> {
+	using type = typename std::vector<T>;
+};
+
+template <typename T, size_t N>
+auto make_vector_impl(const std::vector<std::size_t>& ls, T init_value) {
+	if constexpr(N == 0) {
+		return std::vector<T>(ls[N], init_value);
+	} else {
+		return typename make_vector_type<T, N>::type(
+			ls[N], make_vector_impl<T, (N - 1)>(ls, init_value));
 	}
-	scanf("%d %d", &s, &t);
+}
 
-	queue<int> q;
-	q.push(s);
+template <typename T, std::size_t N>
+auto make_vector(const std::size_t (&ls)[N], T init_value) {
+	std::vector<std::size_t> dimensions(N);
+	for(int i = 0; i < N; i++) {
+		dimensions[N - i - 1] = ls[i];
+	}
+	return make_vector_impl<T, N - 1>(dimensions, init_value);
+}
 
-	int count = 0;
-	for(;;) {
-		TIMEUP;
-		if(q.empty()) {
-			printf("%d\n", -1);
-			return 0;
-		}
+template <typename T>
+std::vector<T> make_vector(std::size_t size, T init_value) {
+	return std::vector<T>(size, init_value);
+}
 
-		int node = q.front();
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T, typename U>
+constexpr void chmax(T& m, U x) {
+	m = max<T>(m, x);
+}
+
+template <typename T, typename U>
+constexpr void chmin(T& m, U x) {
+	m = min<T>(m, x);
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+using Weight = int;
+
+// 辺
+struct Edge {
+	size_t from;
+	size_t to;
+	Weight cost;
+	Edge(size_t t, Weight c) : to(t), cost(c) {}
+	Edge(size_t f, size_t t, Weight c) : from(f), to(t), cost(c) {}
+	bool operator<(const Edge& rhs) const { return this->cost > rhs.cost; }
+};
+
+// グラフ G=(V,E)
+struct Graph {
+	size_t node;
+	std::vector<std::vector<Edge>> edges;
+
+	Graph(size_t n) : node(n), edges(n) {}
+	void link(size_t from, size_t to, Weight cost = 1) {
+		edges[from].push_back(Edge(from, to, cost));
+	}
+};
+
+std::vector<Weight> bfs(const Graph& graph, const size_t s) {
+	size_t n = graph.node;
+	std::vector<bool> used(n, false);
+	std::vector<Weight> distances(n, INF);
+
+	distances[s] = 0;
+	used[s] = true;
+	std::queue<Edge> q;
+	q.push(Edge(s, s, 0));
+	while(!q.empty()) {
+		Edge edge = q.front();
 		q.pop();
-
-		// printf("node: %d\n", node);
-
-		if(node == t) {
-			break;
-		}
-
-		count++;
-
-		{
-			queue<int> q2[4];
-			q2[0].push(node);
-
-			for(int i = 0; i < 3; i++) {
-				while(!q2[i].empty()) {
-					int now = q2[i].front();
-					q2[i].pop();
-					// printf("now: %d\n", now);
-					for(int j = 1; j <= n; j++) {
-						if(edge[now][j] == 1) {
-							// printf("q2[%d+1].push(j)\n", i);
-							q2[i + 1].push(j);
-						}
-					}
-				}
+		for(auto&& e : graph.edges[edge.to]) {
+			if(used[e.to]) {
+				continue;
 			}
-
-			while(!q2[3].empty()) {
-				// printf("q.push(q2.front())\n");
-				q.push(q2[3].front());
-				q2[3].pop();
-			}
+			distances[e.to] = distances[e.from] + 1;
+			q.push(Edge(e.from, e.to, 1));
+			used[e.to] = true;
 		}
 	}
+	return distances;
+}
 
-	printf("%d\n", count);
+void solve(Graph& g1, Graph& g2, int s, int t) {}
 
+int main() {
+	int n, m;
+	cin >> n >> m;
+	VI u(m);
+	VI v(m);
+	Graph graph(n * 3);
+	REP(i, m) {
+		cin >> u[i] >> v[i];
+		u[i]--;
+		v[i]--;
+	}
+	int s, t;
+	cin >> s >> t;
+	s--;
+	t--;
+
+	REP(i, m) {
+		graph.link(u[i] + n * 0, v[i] + n * 1);
+		graph.link(u[i] + n * 1, v[i] + n * 2);
+		graph.link(u[i] + n * 2, v[i] + n * 0);
+	}
+	auto distances = bfs(graph, s);
+
+	cout << (distances[t] == INF ? -1 : distances[t] / 3) << endl;
 	return 0;
 }
