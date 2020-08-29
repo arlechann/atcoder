f3141e6d05e621d38810fe010c69a687d4d6d714
arlechann <dragnov3728@gmail.com>
2020-08-29 23:27:51 +0900

    Add UnionFind::size()

--- library/unionfind.cpp
+++ library/unionfind.cpp
@@ -6,9 +6,10 @@
 class UnionFind {
 	std::vector<size_t> parents;
 	std::vector<size_t> rank;
+	std::vector<size_t> tree_size;
 
 	public:
-	UnionFind(size_t size) : parents(size), rank(size, 0) {
+	UnionFind(size_t size) : parents(size), rank(size, 0), tree_size(size, 1) {
 		std::iota(this->parents.begin(), this->parents.end(), 0);
 	}
 
@@ -25,6 +26,7 @@ class UnionFind {
 		if(this->rank[ar] == this->rank[br]) {
 			this->rank[ar]++;
 		}
+		this->tree_size[ar] += this->tree_size[br];
 		return this->parents[br] = ar;
 	}
 	bool unite(size_t a, size_t b) { return this->merge(a, b); }
@@ -32,6 +34,9 @@ class UnionFind {
 	// 同集合か判定
 	bool is_same(size_t a, size_t b) { return this->root(a) == this->root(b); }
 	bool is_union(size_t a, size_t b) { return this->is_same(a, b); }
+
+	// 集合の大きさ
+	size_t size(size_t n) { return this->tree_size[this->root(n)]; }
 
 	private:
 	size_t root(int n) {
