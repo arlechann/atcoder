7d658116fb941f578ef8ac1fe2f503b505551163
arlechann <dragnov3728@gmail.com>
2020-10-17 18:27:57 +0900

    Add rust template

---
+++ library/template.rs
@@ -0,0 +1,67 @@
+macro_rules! read_line {
+	($(let $i:ident : $t:ty);*;) => {
+		let mut buf = String::new();
+		std::io::stdin().read_line(&mut buf).unwrap();
+		let mut iter = buf.split_whitespace();
+		$(
+			let $i: $t = iter.next().unwrap().parse().unwrap();
+		)*
+	};
+}
+
+#[allow(dead_code)]
+struct CumulativeSum<I: Iterator> {
+	next: Option<usize>,
+	underlying: I
+}
+
+#[allow(dead_code)]
+impl<I> Iterator for CumulativeSum<I>
+where
+	I: Iterator,
+	I::Item: Into<usize>
+{
+	type Item = usize;
+
+	fn next(&mut self) -> Option<usize> {
+		match (self.underlying.next(), self.next) {
+			(Some(x), Some(y)) => {
+				self.next = Some(x.into() + y);
+				Some(y)
+			},
+			(None, Some(y)) => {
+				self.next = None;
+				Some(y)
+			},
+			_ => None
+		}
+	}
+}
+
+#[allow(dead_code)]
+trait CumulativeSumExt: Iterator {
+	fn cumulative_sum(self) -> CumulativeSum<Self>
+	where
+		Self: Sized,
+		Self::Item: Into<usize>
+	{
+		CumulativeSum {
+			next: Some(0usize),
+			underlying: self
+		}
+	}
+}
+
+#[allow(dead_code)]
+impl<I: Iterator> CumulativeSumExt for I {}
+
+#[allow(dead_code)]
+const MOD: usize = 1_000_000_007;
+
+fn main() {
+	read_line!{}
+
+	println!("{}", solve());
+}
+
+fn solve() -> u32 {}
