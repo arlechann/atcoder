9b73fa24f805f01452b4b0d8b8053b35003c2c22
arlechann <dragnov3728@gmail.com>
2020-10-27 02:33:03 +0900

    Update template.rs

--- library/template.rs
+++ library/template.rs
@@ -1,45 +1,76 @@
-#[allow(unused_imports)]
-use std::cmp::max;
-#[allow(unused_imports)]
-use std::cmp::min;
-#[allow(unused_imports)]
-use std::fmt::Debug;
-#[allow(unused_imports)]
-use std::io::{stdin, Read};
-
-#[allow(unused_macros)]
-macro_rules! read {
-	($i:ident) => {
-		$i.next().unwrap().parse().unwrap();
-	};
-	($i:ident, $t:ty) => {
-		$i.next().unwrap().parse::<$t>().unwrap();
-	};
-}
-
-#[allow(unused_macros)]
-macro_rules! read_init {
-	($i:ident) => {
-		read_init!(stdin, $i)
-	};
-	(stdin, $i:ident) => {
-		let mut _buf = String::new();
-		stdin().read_to_string(&mut _buf).unwrap();
-		let mut $i = _buf.split_whitespace();
-	};
+fn main() {
+	let mut solver = solve::Solver::new();
+	solver.input();
+	println!("{}", solver.solve());
 }
 
 #[allow(dead_code)]
-const MOD: u64 = 1_000_000_007;
+mod solve {
+	use super::*;
+	#[allow(unused_imports)]
+	use std::cmp::max;
+	#[allow(unused_imports)]
+	use std::cmp::min;
+	#[allow(unused_imports)]
+	use std::fmt::Debug;
 
-fn main() {
-	read_init!(buf);
+	const MOD: u64 = 1_000_000_007;
 
-	println!("{}", solve());
+	#[derive(Default)]
+	pub struct Solver {
+		input: input::Input,
+	}
+
+	impl Solver {
+		pub fn new() -> Self {
+			Default::default()
+		}
+
+		pub fn input(&mut self) {
+			todo!();
+		}
+
+		pub fn solve(&self) -> usize {
+			todo!();
+		}
+	}
 }
 
-fn solve() -> usize {
-	todo!()
+#[allow(dead_code)]
+mod input {
+	use std::fmt::Debug;
+	use std::io::{stdin, Read};
+	use std::str::FromStr;
+	static mut _INPUT_BUF: String = String::new();
+
+	pub struct Input {
+		buf: &'static str,
+		input_iter: Box<dyn Iterator<Item = &'static str>>,
+	}
+
+	impl Input {
+		pub fn read<T>(&mut self) -> T
+		where
+			T: FromStr,
+			T::Err: Debug,
+		{
+			self.input_iter.next().unwrap().parse().unwrap()
+		}
+	}
+
+	impl Default for Input {
+		fn default() -> Self {
+			unsafe {
+				stdin().read_to_string(&mut _INPUT_BUF).unwrap();
+				let input_iter: Box<dyn Iterator<Item = &str>> =
+					Box::new(_INPUT_BUF.split_whitespace());
+				Self {
+					buf: &_INPUT_BUF,
+					input_iter,
+				}
+			}
+		}
+	}
 }
 
 #[allow(dead_code)]
@@ -77,45 +108,68 @@ mod imos {
 
 #[allow(dead_code)]
 mod iter_utils {
-	struct CumulativeSum<I: Iterator> {
-		next: Option<u64>,
-		underlying: I,
-	}
-
-	impl<I> Iterator for CumulativeSum<I>
-	where
-		I: Iterator,
-		I::Item: Into<u64>,
-	{
-		type Item = u64;
-
-		fn next(&mut self) -> Option<u64> {
-			match (self.underlying.next(), self.next) {
-				(Some(x), Some(y)) => {
-					self.next = Some(x.into() + y);
-					Some(y)
-				}
-				(None, Some(y)) => {
-					self.next = None;
-					Some(y)
-				}
-				_ => None,
-			}
+	pub mod cumulative_sum {
+		struct CumulativeSum<I: Iterator> {
+			next: Option<u64>,
+			underlying: I,
 		}
-	}
 
-	trait CumulativeSumExt: Iterator {
-		fn cumulative_sum(self) -> CumulativeSum<Self>
+		impl<I> Iterator for CumulativeSum<I>
 		where
-			Self: Sized,
-			Self::Item: Into<u64>,
+			I: Iterator,
+			I::Item: Into<u64>,
 		{
-			CumulativeSum {
-				next: Some(0u64),
-				underlying: self,
+			type Item = u64;
+
+			fn next(&mut self) -> Option<u64> {
+				match (self.underlying.next(), self.next) {
+					(Some(x), Some(y)) => {
+						self.next = Some(x.into() + y);
+						Some(y)
+					}
+					(None, Some(y)) => {
+						self.next = None;
+						Some(y)
+					}
+					_ => None,
+				}
 			}
 		}
+
+		trait CumulativeSumExt: Iterator {
+			fn cumulative_sum(self) -> CumulativeSum<Self>
+			where
+				Self: Sized,
+				Self::Item: Into<u64>,
+			{
+				CumulativeSum {
+					next: Some(0u64),
+					underlying: self,
+				}
+			}
+		}
+
+		impl<I: Iterator> CumulativeSumExt for I {}
 	}
 
-	impl<I: Iterator> CumulativeSumExt for I {}
+	pub mod counter {
+		use std::collections::HashMap;
+		use std::hash::Hash;
+
+		pub trait CounterExt: Iterator {
+			fn counter(mut self) -> HashMap<Self::Item, usize>
+			where
+				Self: Sized,
+				Self::Item: Eq + PartialEq + Hash,
+			{
+				let mut map = HashMap::new();
+				while let Some(e) = self.next() {
+					*map.entry(e).or_insert(0) += 1;
+				}
+				map
+			}
+		}
+
+		impl<I: Iterator> CounterExt for I {}
+	}
 }
