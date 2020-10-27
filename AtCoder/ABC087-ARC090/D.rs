958d4d7951b443ae31f91de712ac25f9ea734f0c
arlechann <dragnov3728@gmail.com>
2020-10-27 23:16:22 +0900

    ABC087/ARC090 D問題 AC

---
+++ AtCoder/ABC087-ARC090/D.rs
@@ -0,0 +1,182 @@
+fn main() {
+	let mut solver = solve::Solver::new();
+	solver.input();
+	println!("{}", solver.solve());
+}
+
+#[allow(dead_code)]
+mod solve {
+	use super::*;
+	#[allow(unused_imports)]
+	use std::cmp::max;
+	#[allow(unused_imports)]
+	use std::cmp::min;
+	#[allow(unused_imports)]
+	use std::fmt::Debug;
+
+	const MOD: u64 = 1_000_000_007;
+
+	#[derive(Default)]
+	pub struct Solver {
+		n: usize,
+		m: usize,
+		lrd: Vec<(usize, usize, usize)>,
+		input: input::Input,
+	}
+
+	impl Solver {
+		pub fn new() -> Self {
+			Default::default()
+		}
+
+		pub fn input(&mut self) {
+			self.n = self.input.read();
+			self.m = self.input.read();
+			self.lrd = (0..self.m)
+				.map(|_| (self.input.read(), self.input.read(), self.input.read()))
+				.collect();
+		}
+
+		pub fn solve(&self) -> usize {
+			todo!();
+		}
+	}
+}
+
+#[allow(dead_code)]
+mod input {
+	use std::fmt::Debug;
+	use std::io::{stdin, Read};
+	use std::str::FromStr;
+	static mut _INPUT_BUF: String = String::new();
+
+	pub struct Input {
+		buf: &'static str,
+		input_iter: Box<dyn Iterator<Item = &'static str>>,
+	}
+
+	impl Input {
+		pub fn read<T>(&mut self) -> T
+		where
+			T: FromStr,
+			T::Err: Debug,
+		{
+			self.input_iter.next().unwrap().parse().unwrap()
+		}
+	}
+
+	impl Default for Input {
+		fn default() -> Self {
+			unsafe {
+				stdin().read_to_string(&mut _INPUT_BUF).unwrap();
+				let input_iter: Box<dyn Iterator<Item = &str>> =
+					Box::new(_INPUT_BUF.split_whitespace());
+				Self {
+					buf: &_INPUT_BUF,
+					input_iter,
+				}
+			}
+		}
+	}
+}
+
+#[allow(dead_code)]
+mod imos {
+	struct Imos {
+		array: Vec<i64>,
+		len: usize,
+	}
+
+	impl Imos {
+		fn with_capacity(len: usize) -> Self {
+			Self {
+				array: vec![0; len],
+				len: len,
+			}
+		}
+
+		fn query(&mut self, left: usize, right: usize, value: i64) {
+			assert!(left < right);
+			self.array[left] += value;
+			self.array[right] -= value;
+		}
+
+		fn apply(&self) -> Vec<i64> {
+			self.array
+				.iter()
+				.scan(0i64, |s, &e| {
+					*s = *s + e;
+					Some(*s)
+				})
+				.collect()
+		}
+	}
+}
+
+#[allow(dead_code)]
+mod iter_utils {
+	pub mod cumulative_sum {
+		struct CumulativeSum<I: Iterator> {
+			next: Option<u64>,
+			underlying: I,
+		}
+
+		impl<I> Iterator for CumulativeSum<I>
+		where
+			I: Iterator,
+			I::Item: Into<u64>,
+		{
+			type Item = u64;
+
+			fn next(&mut self) -> Option<u64> {
+				match (self.underlying.next(), self.next) {
+					(Some(x), Some(y)) => {
+						self.next = Some(x.into() + y);
+						Some(y)
+					}
+					(None, Some(y)) => {
+						self.next = None;
+						Some(y)
+					}
+					_ => None,
+				}
+			}
+		}
+
+		trait CumulativeSumExt: Iterator {
+			fn cumulative_sum(self) -> CumulativeSum<Self>
+			where
+				Self: Sized,
+				Self::Item: Into<u64>,
+			{
+				CumulativeSum {
+					next: Some(0u64),
+					underlying: self,
+				}
+			}
+		}
+
+		impl<I: Iterator> CumulativeSumExt for I {}
+	}
+
+	pub mod counter {
+		use std::collections::HashMap;
+		use std::hash::Hash;
+
+		pub trait CounterExt: Iterator {
+			fn counter(mut self) -> HashMap<Self::Item, usize>
+			where
+				Self: Sized,
+				Self::Item: Eq + PartialEq + Hash,
+			{
+				let mut map = HashMap::new();
+				while let Some(e) = self.next() {
+					*map.entry(e).or_insert(0) += 1;
+				}
+				map
+			}
+		}
+
+		impl<I: Iterator> CounterExt for I {}
+	}
+}
