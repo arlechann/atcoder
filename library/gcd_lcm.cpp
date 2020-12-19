b6dab919c6f4493dd93fa56ab37925fd0f86bc27
arlechann <dragnov3728@gmail.com>
2020-12-20 00:26:41 +0900

    Add ext_gcd

--- library/gcd_lcm.cpp
+++ library/gcd_lcm.cpp
@@ -22,3 +22,12 @@ constexpr long long gcd(long long a, long long b) {
 constexpr long long lcm(long long a, long long b) {
 	return a / gcd(a, b) * b;
 }
+
+// 拡張ユークリッド互除法
+tuple<long long, long long, long long> ext_gcd(long long a, long long b) {
+	if(b == 0) {
+		return std::make_tuple(1, 0, a);
+	}
+	auto [y, x, d] = ext_gcd(b, a % b);
+	return std::make_tuple(x, y - a / b * x, d);
+}
