13f13d3694a20926f86135331e4057b334ce15ef
arlechann <dragnov3728@gmail.com>
2021-04-28 21:35:59 +0900

    ABC145 E問題 解説AC

--- AtCoder/ABC145/E.cpp
+++ AtCoder/ABC145/E.cpp
@@ -4,60 +4,136 @@
 #include <cstdio>
 #include <cstring>
 #include <functional>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <list>
+#include <map>
 #include <numeric>
 #include <queue>
+#include <set>
 #include <sstream>
 #include <string>
 #include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
-#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < (i##_MACRO); i++)
-#define RANGE(i, a, b) for(int i = (a), i < (b), i++)
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
 #define EACH(e, a) for(auto&& e : a)
-#define ALL(a) (a).begin(), (a).end()
-#define AALL(a, n) (a), a + n
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
 #define FILL(a, n) memset((a), n, sizeof(a))
 #define FILLZ(a) FILL(a, 0)
-#define MOD(x) ((x) % static_cast<int>(1e9 + 7))
+#define INT(x) (static_cast<int>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
 
 using namespace std;
 
 using ll = long long;
-using VI = vector<int>;
-using VI2D = vector<vector<int>>;
+using VI = std::vector<int>;
+using VI2D = std::vector<vector<int>>;
+using VLL = std::vector<long long>;
+using VLL2D = std::vector<vector<long long>>;
 
-const int INF = 2e9;
-const double EPS = 1e-10;
-const double PI = acos(-1.0);
+constexpr int INF = 2e9;
+constexpr long long INFLL = 2e18;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
 
-inline int toInt(string s) {
-	int v;
-	istringstream sin(s);
-	sin >> v;
-	return v;
+constexpr int dx[] = {-1, 0, 1, 0};
+constexpr int dy[] = {0, -1, 0, 1};
+
+template <typename T, std::size_t N>
+struct make_vector_type {
+	using type =
+		typename std::vector<typename make_vector_type<T, (N - 1)>::type>;
+};
+
+template <typename T>
+struct make_vector_type<T, 0> {
+	using type = typename std::vector<T>;
+};
+
+template <typename T, size_t N>
+auto make_vector_impl(const std::vector<std::size_t>& ls, T init_value) {
+	if constexpr(N == 0) {
+		return std::vector<T>(ls[N], init_value);
+	} else {
+		return typename make_vector_type<T, N>::type(
+			ls[N], make_vector_impl<T, (N - 1)>(ls, init_value));
+	}
 }
 
-int dp[3001][3001];
+template <typename T, std::size_t N>
+auto make_vector(const std::size_t (&ls)[N], T init_value) {
+	std::vector<std::size_t> dimensions(N);
+	for(int i = 0; i < N; i++) {
+		dimensions[N - i - 1] = ls[i];
+	}
+	return make_vector_impl<T, N - 1>(dimensions, init_value);
+}
+
+template <typename T>
+std::vector<T> make_vector(std::size_t size, T init_value) {
+	return std::vector<T>(size, init_value);
+}
+
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T, typename U>
+constexpr void chmax(T& m, U x) {
+	m = max<T>(m, x);
+}
+
+template <typename T, typename U>
+constexpr void chmin(T& m, U x) {
+	m = min<T>(m, x);
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+template <typename T>
+constexpr T diff(T a, T b) {
+	return abs(a - b);
+}
 
 int main() {
 	int n, t;
-	scanf("%d %d", &n, &t);
-	VI a(n);
-	VI b(n);
-	REP(i, n) { scanf("%d %d", &a[i], &b[i]); }
+	cin >> n >> t;
+	vector<pair<int, int>> ab(n);
+	REP(i, n) { cin >> ab[i].first >> ab[i].second; }
 
+	sort(ALL(ab), [](auto a, auto b) { return a.first < b.first; });
+	VI2D dp(n + 1, VI(t + 1, 0));
 	REP(i, n) {
-		REP(j, t + 1) {
-			if(j + a[i] < 3001) {
-				dp[i + 1][j + a[i]] = max(dp[i][j] + b[i], dp[i + 1][j + a[i]]);
+		REP(j, t) {
+			if(j >= ab[i].first) {
+				dp[i + 1][j] =
+					max(dp[i][j - ab[i].first] + ab[i].second, dp[i][j]);
+			} else {
+				dp[i + 1][j] = dp[i][j];
 			}
 		}
+		dp[i + 1][t] = dp[i][t - 1] + ab[i].second;
 	}
 
-	printf("%d\n", dp[n][t]);
-
+	int result = 0;
+	REP(i, n + 1) { chmax(result, dp[i][t]); }
+	cout << result << endl;
 	return 0;
 }
