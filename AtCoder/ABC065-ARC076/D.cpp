8ab36b1be3709973c2245d09f4e12a60705b8bf8
arlechann <dragnov3728@gmail.com>
2021-07-01 22:37:11 +0900

    ABC065-ARC076 D問題 AC

---
+++ AtCoder/ABC065-ARC076/D.cpp
@@ -0,0 +1,214 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RREP(i, n) for(int i = (n)-1; i >= 0; i--)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define RRANGE(i, a, b) for(int i = (b)-1, i##_MACRO = (a); i >= i##_MACRO; i--)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define INT(x) (static_cast<int>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
+
+using namespace std;
+
+using ll = long long;
+using VI = std::vector<int>;
+using VI2D = std::vector<vector<int>>;
+using VLL = std::vector<long long>;
+using VLL2D = std::vector<vector<long long>>;
+
+constexpr int INF = 2e9;
+constexpr long long INFLL = 2e18;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
+
+constexpr int dx[] = {-1, 0, 1, 0};
+constexpr int dy[] = {0, -1, 0, 1};
+
+template <typename T, std::size_t N>
+struct make_vector_type {
+	using type =
+		typename std::vector<typename make_vector_type<T, (N - 1)>::type>;
+};
+
+template <typename T>
+struct make_vector_type<T, 0> {
+	using type = typename std::vector<T>;
+};
+
+template <typename T, size_t N>
+auto make_vector_impl(const std::vector<std::size_t>& ls, T init_value) {
+	if constexpr(N == 0) {
+		return std::vector<T>(ls[N], init_value);
+	} else {
+		return typename make_vector_type<T, N>::type(
+			ls[N], make_vector_impl<T, (N - 1)>(ls, init_value));
+	}
+}
+
+template <typename T, std::size_t N>
+auto make_vector(const std::size_t (&ls)[N], T init_value) {
+	std::vector<std::size_t> dimensions(N);
+	for(int i = 0; i < N; i++) {
+		dimensions[N - i - 1] = ls[i];
+	}
+	return make_vector_impl<T, N - 1>(dimensions, init_value);
+}
+
+template <typename T>
+std::vector<T> make_vector(std::size_t size, T init_value) {
+	return std::vector<T>(size, init_value);
+}
+
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T, typename U>
+constexpr bool chmax(T& m, U x) {
+	m = max<T>(m, x);
+	return m < x;
+}
+
+template <typename T, typename U>
+constexpr bool chmin(T& m, U x) {
+	m = min<T>(m, x);
+	return m > x;
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+template <typename T>
+constexpr T pow(T a, int n) {
+	T ret = 1;
+	while(n != 0) {
+		if(n % 2) {
+			ret *= a;
+		}
+		a *= a;
+		n /= 2;
+	}
+	return ret;
+}
+
+template <typename T>
+constexpr T diff(T a, T b) {
+	return abs(a - b);
+}
+
+using Weight = int;
+
+// 辺
+struct Edge {
+	size_t from;
+	size_t to;
+	Weight cost;
+	Edge(size_t t, Weight c) : to(t), cost(c) {}
+	Edge(size_t f, size_t t, Weight c) : from(f), to(t), cost(c) {}
+	bool operator<(const Edge& rhs) const { return this->cost > rhs.cost; }
+};
+
+// グラフ G=(V,E)
+struct Graph {
+	size_t node;
+	std::vector<std::vector<Edge>> edges;
+
+	Graph(size_t n) : node(n), edges(n) {}
+	void link(size_t from, size_t to, Weight cost = 1) {
+		edges[from].push_back(Edge(from, to, cost));
+		edges[to].push_back(Edge(to, from, cost));
+	}
+};
+
+// 最小全域木
+std::pair<Weight, Graph> prim(const Graph& graph) {
+	size_t n = graph.node;
+	std::vector<bool> used(n, false);
+	Weight total = 0;
+	Graph mst(n);
+
+	std::priority_queue<Edge> pq;
+	pq.push(Edge(-1, 0, 0));
+	while(!pq.empty()) {
+		Edge edge = pq.top();
+		pq.pop();
+		if(used[edge.to]) {
+			continue;
+		}
+		used[edge.to] = true;
+		total += edge.cost;
+		if(edge.from != -1) {
+			mst.edges[edge.from].push_back(edge);
+		}
+		for(auto&& e : graph.edges[edge.to]) {
+			if(!used[e.to]) {
+				pq.push(e);
+			}
+		}
+	}
+
+	return std::pair<Weight, Graph>(total, mst);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	VI x(n), y(n);
+	REP(i, n) { cin >> x[i] >> y[i]; }
+
+	vector<pair<int, int>> sorted_x(n);
+	REP(i, n) { sorted_x[i] = make_pair(x[i], i); }
+	sort(ALL(sorted_x));
+
+	vector<pair<int, int>> sorted_y(n);
+	REP(i, n) { sorted_y[i] = make_pair(y[i], i); }
+	sort(ALL(sorted_y));
+
+	Graph g(n);
+	REP(i, n - 1) {
+		g.link(sorted_x[i].second,
+			   sorted_x[i + 1].second,
+			   sorted_x[i + 1].first - sorted_x[i].first);
+		g.link(sorted_y[i].second,
+			   sorted_y[i + 1].second,
+			   sorted_y[i + 1].first - sorted_y[i].first);
+	}
+
+	auto [cost, mintree] = prim(g);
+
+	cout << cost << endl;
+	return 0;
+}
