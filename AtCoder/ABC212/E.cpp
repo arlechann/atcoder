a0aae73c65f725ec1611cea02a4a22a31d51c6a8
arlechann <dragnov3728@gmail.com>
2021-07-31 22:45:08 +0900

    ABC212

---
+++ AtCoder/ABC212/E.cpp
@@ -0,0 +1,357 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RREP(i, n) for(int i = (n)-1; i >= 0; i--)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define RRANGE(i, a, b) for(int i = (b)-1, i##_MACRO = (a); i >= i##_MACRO; i--)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define INT(x) (static_cast<int>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
+
+using namespace std;
+
+using ll = long long;
+using VI = std::vector<int>;
+using VI2D = std::vector<vector<int>>;
+using VLL = std::vector<long long>;
+using VLL2D = std::vector<vector<long long>>;
+
+constexpr int INF = 2e9;
+constexpr long long INFLL = 2e18;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
+
+constexpr int dx[] = {-1, 0, 1, 0};
+constexpr int dy[] = {0, -1, 0, 1};
+
+template <typename T, std::size_t N>
+struct make_vector_type {
+	using type =
+		typename std::vector<typename make_vector_type<T, (N - 1)>::type>;
+};
+
+template <typename T>
+struct make_vector_type<T, 0> {
+	using type = typename std::vector<T>;
+};
+
+template <typename T, size_t N>
+auto make_vector_impl(const std::vector<std::size_t>& ls, T init_value) {
+	if constexpr(N == 0) {
+		return std::vector<T>(ls[N], init_value);
+	} else {
+		return typename make_vector_type<T, N>::type(
+			ls[N], make_vector_impl<T, (N - 1)>(ls, init_value));
+	}
+}
+
+template <typename T, std::size_t N>
+auto make_vector(const std::size_t (&ls)[N], T init_value) {
+	std::vector<std::size_t> dimensions(N);
+	for(int i = 0; i < N; i++) {
+		dimensions[N - i - 1] = ls[i];
+	}
+	return make_vector_impl<T, N - 1>(dimensions, init_value);
+}
+
+template <typename T>
+std::vector<T> make_vector(std::size_t size, T init_value) {
+	return std::vector<T>(size, init_value);
+}
+
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T, typename U>
+constexpr bool chmax(T& m, U x) {
+	m = max<T>(m, x);
+	return m < x;
+}
+
+template <typename T, typename U>
+constexpr bool chmin(T& m, U x) {
+	m = min<T>(m, x);
+	return m > x;
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+template <typename T>
+constexpr T pow(T a, int n) {
+	T ret = 1;
+	while(n != 0) {
+		if(n % 2) {
+			ret *= a;
+		}
+		a *= a;
+		n /= 2;
+	}
+	return ret;
+}
+
+template <typename T>
+constexpr T diff(T a, T b) {
+	return abs(a - b);
+}
+
+template <long long MOD = 1000000007>
+class ModInt {
+	public:
+	long long n;
+
+	constexpr ModInt() : n(0) {}
+	constexpr ModInt(long long n) : n(n < 0 ? n + MOD : n % MOD) {}
+
+	constexpr long long get() const { return this->n; }
+	constexpr long long get_mod() const { return MOD; }
+
+	constexpr ModInt inv() const { return pow<ModInt<MOD>>(*this, MOD - 2); }
+
+	constexpr ModInt& operator=(const long long rhs) {
+		return *this = ModInt(rhs);
+	}
+	constexpr ModInt& operator+=(const ModInt rhs) {
+		return *this = ModInt(this->n + rhs.n);
+	}
+	constexpr ModInt& operator-=(const ModInt rhs) {
+		return *this = ModInt(this->n - rhs.n);
+	}
+	constexpr ModInt& operator*=(const ModInt rhs) {
+		return *this = ModInt(this->n * rhs.n);
+	}
+	constexpr ModInt& operator/=(const ModInt rhs) {
+		return *this *= rhs.inv();
+	}
+	constexpr bool operator==(const ModInt rhs) const {
+		return this->n == rhs.n;
+	}
+};
+
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator*(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) *= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator*(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) *= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator*(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) *= rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator/(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) /= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator/(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) /= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator/(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) /= rhs;
+}
+
+template <long long MOD>
+std::ostream& operator<<(std::ostream& os, const ModInt<MOD>& x) {
+	return os << x.n;
+}
+
+template <long long MOD>
+std::istream& operator>>(std::istream& is, ModInt<MOD>& x) {
+	return is >> x.n;
+}
+
+using mint = ModInt<998244353>;
+
+int roundup_pow2(int n) {
+	if(!(n & (n - 1))) {
+		return n;
+	}
+
+	int i = 1;
+	while((n >> i) != 0) {
+		i++;
+	}
+	return 1 << i;
+}
+
+// セグメント木(一点更新、区間取得)
+template <typename T>
+class SegmentTree {
+	using F = function<T(T, T)>;
+
+	// 演算
+	F merge;
+	// 単位元
+	T identity;
+	vector<T> tree;
+	size_t size;
+
+	public:
+	SegmentTree(const vector<T> a, const F f, const T id)
+		: tree(roundup_pow2(a.size()) * 2 - 1, id),
+		  size(roundup_pow2(a.size())), merge(f), identity(id) {
+		int offset = this->size - 1;
+		for(int i = 0; i < a.size(); i++) {
+			this->tree[i + offset] = a[i];
+		}
+		for(int i = offset - 1; i >= 0; i--) {
+			this->tree[i] = this->apply(i);
+		}
+	}
+	// モノイド(Z,+)
+	SegmentTree(const vector<T> a)
+		: SegmentTree(
+			  a, [](T a, T b) { return a + b; }, 0) {}
+
+	// 更新
+	// 関数の指定がなければ置き換え
+	void update(
+		const size_t index, const T value, const F f = [](T a, T b) {
+			return b;
+		}) {
+		size_t i = index + size - 1;
+		this->tree[i] = f(this->tree[i], value);
+		while(i > 0) {
+			i = (i - 1) / 2;
+			this->tree[i] = this->apply(i);
+		}
+	}
+
+	// 一点取得
+	T find(const size_t index) { return this->tree[index + size - 1]; }
+
+	// 区間取得
+	T find(const size_t query_left, const size_t query_right) const {
+		return this->find_impl(query_left, query_right, 0, 0, this->size);
+	}
+
+	private:
+	T apply(size_t index) const {
+		return this->merge(this->tree[index * 2 + 1],
+						   this->tree[index * 2 + 2]);
+	}
+
+	T find_impl(size_t query_left,
+				size_t query_right,
+				size_t node,
+				size_t node_left,
+				size_t node_right) const {
+		if(node_right <= query_left || query_right <= node_left) {
+			return this->identity;
+		}
+		if(query_left <= node_left && node_right <= query_right) {
+			return this->tree[node];
+		}
+
+		return this->merge(find_impl(query_left,
+									 query_right,
+									 node * 2 + 1,
+									 node_left,
+									 node_left + (node_right - node_left) / 2),
+						   find_impl(query_left,
+									 query_right,
+									 node * 2 + 2,
+									 node_left + (node_right - node_left) / 2,
+									 node_right));
+	}
+};
+
+int main() {
+	int n, m, k;
+	cin >> n >> m >> k;
+	VI u(m), v(m);
+	REP(i, m) {
+		cin >> u[i] >> v[i];
+		u[i]--;
+		v[i]--;
+	}
+
+	VI2D noedges(n);
+	REP(i, m) {
+		noedges[u[i]].push_back(v[i]);
+		noedges[v[i]].push_back(u[i]);
+	}
+
+	vector<mint> a(n, 0);
+	vector<SegmentTree<mint>> dp(k + 1, SegmentTree<mint>(a));
+	dp[0].update(0, 1);
+	REP(i, k) {
+		REP(j, n) {
+			mint x = dp[i].find(0, n);
+			x -= dp[i].find(j);
+			EACH(e, noedges[j]) { x -= dp[i].find(e); }
+			dp[i + 1].update(j, x);
+		}
+	}
+
+	cout << dp[k].find(0) << endl;
+	return 0;
+}
