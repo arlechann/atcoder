758ad129a54db440bca4145a5caa6dcada78727e
arlechann <dragnov3728@gmail.com>
2022-04-09 01:47:44 +0900

    update graph.cpp

--- AOJ/GRL/GRL_1_C.cpp
+++ AOJ/GRL/GRL_1_C.cpp
@@ -1,9 +1,136 @@
 #include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
 #include <limits>
+#include <list>
+#include <map>
+#include <numeric>
 #include <queue>
+#include <set>
+#include <sstream>
 #include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RREP(i, n) for(int i = (n)-1; i >= 0; i--)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define RRANGE(i, a, b) for(int i = (b)-1, i##_MACRO = (a); i >= i##_MACRO; i--)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define CAST(x, t) (static_cast<t>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
+
+using namespace std;
+
+using ll = long long;
+using VI = vector<int>;
+using VI2D = vector<vector<int>>;
+using VLL = vector<long long>;
+using VLL2D = vector<vector<long long>>;
+
+constexpr int INF = 2e9;
+constexpr long long INFLL = 2e18;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
+
+template <typename T, std::size_t N>
+struct make_vector_type {
+	using type =
+		typename std::vector<typename make_vector_type<T, (N - 1)>::type>;
+};
+
+template <typename T>
+struct make_vector_type<T, 0> {
+	using type = typename std::vector<T>;
+};
+
+template <typename T, size_t N>
+auto make_vector_impl(const std::vector<std::size_t>& ls, T init_value) {
+	if constexpr(N == 0) {
+		return std::vector<T>(ls[N], init_value);
+	} else {
+		return typename make_vector_type<T, N>::type(
+			ls[N], make_vector_impl<T, (N - 1)>(ls, init_value));
+	}
+}
+
+template <typename T, std::size_t N>
+auto make_vector(const std::size_t (&ls)[N], T init_value) {
+	std::vector<std::size_t> dimensions(N);
+	for(int i = 0; i < N; i++) {
+		dimensions[N - i - 1] = ls[i];
+	}
+	return make_vector_impl<T, N - 1>(dimensions, init_value);
+}
+
+template <typename T>
+std::vector<T> make_vector(std::size_t size, T init_value) {
+	return std::vector<T>(size, init_value);
+}
+
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T>
+constexpr bool chmax(T& m, T x) {
+	if(m >= x) {
+		return false;
+	}
+	m = x;
+	return true;
+}
+
+template <typename T>
+constexpr bool chmin(T& m, T x) {
+	if(m <= x) {
+		return false;
+	}
+	m = x;
+	return true;
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+template <typename T>
+constexpr T pow(T a, int n) {
+	T ret = 1;
+	while(n != 0) {
+		if(n % 2) {
+			ret *= a;
+		}
+		a *= a;
+		n /= 2;
+	}
+	return ret;
+}
+
+template <typename T>
+constexpr T diff(T a, T b) {
+	return max(a, b) - min(a, b);
+}
 
 template <typename W>
 struct Edge {
@@ -447,8 +574,49 @@ std::vector<std::vector<typename G::Weight>> warshall_floyd(const G& graph) {
 	return distances;
 }
 
-using GraphWeight = int;
+using GraphWeight = ll;
 using DirectedListGraph = Graph<ListGraph<GraphWeight>, Directed>;
 using UndirectedListGraph = Graph<ListGraph<GraphWeight>, Undirected>;
 using DirectedMatrixGraph = Graph<MatrixGraph<GraphWeight>, Directed>;
 using UndirectedMatrixGraph = Graph<ListGraph<GraphWeight>, Undirected>;
+
+/**
+ *  _       _                     _        ____
+ * (_)_ __ | |_   _ __ ___   __ _(_)_ __  / /\ \ _
+ * | | '_ \| __| | '_ ` _ \ / _` | | '_ \| |  | (_)
+ * | | | | | |_  | | | | | | (_| | | | | | |  | |_
+ * |_|_| |_|\__| |_| |_| |_|\__,_|_|_| |_| |  | ( )
+ *                                        \_\/_/|/
+ */
+
+int main() {
+	int v, e;
+	cin >> v >> e;
+	VI s(e), t(e);
+	VLL d(e);
+	REP(i, e) { cin >> s[i] >> t[i] >> d[i]; }
+
+	DirectedMatrixGraph graph = DirectedMatrixGraph(v, s, t, d);
+	auto distances = warshall_floyd(graph);
+
+	REP(i, v) {
+		if(distances[i][i] < 0) {
+			cout << "NEGATIVE CYCLE" << endl;
+			return 0;
+		}
+	}
+	REP(i, v) {
+		REP(j, v) {
+			if(j != 0) {
+				cout << " ";
+			}
+			if(distances[i][j] > INF) {
+				cout << "INF";
+			} else {
+				cout << distances[i][j];
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
