4f407fab2743291aa390cf682a0bbe6f056aaa71
arlechann <dragnov3728@gmail.com>
2022-08-21 22:53:36 +0900

    ABC265

---
+++ AtCoder/ABC265/E.cpp
@@ -0,0 +1,331 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define REP(i, n) for(int i = 0, i##_MACRO = (n); i < i##_MACRO; i++)
+#define RREP(i, n) for(int i = (n)-1; i >= 0; i--)
+#define RANGE(i, a, b) for(int i = (a), i##_MACRO = (b); i < i##_MACRO; i++)
+#define RRANGE(i, a, b) for(int i = (b)-1, i##_MACRO = (a); i >= i##_MACRO; i--)
+#define EACH(e, a) for(auto&& e : a)
+#define ALL(a) std::begin(a), std::end(a)
+#define RALL(a) std::rbegin(a), std::rend(a)
+#define FILL(a, n) memset((a), n, sizeof(a))
+#define FILLZ(a) FILL(a, 0)
+#define CAST(x, t) (static_cast<t>(x))
+#define PRECISION(x) std::fixed << std::setprecision(x)
+
+using namespace std;
+
+using ll = long long;
+using VI = vector<int>;
+using VI2D = vector<vector<int>>;
+using VLL = vector<long long>;
+using VLL2D = vector<vector<long long>>;
+
+constexpr int INF = 2e9;
+constexpr long long INFLL = 2e18;
+constexpr double EPS = 1e-10;
+constexpr double PI = acos(-1.0);
+
+template <typename T, std::size_t N>
+struct make_vector_type {
+	using type =
+		typename std::vector<typename make_vector_type<T, (N - 1)>::type>;
+};
+
+template <typename T>
+struct make_vector_type<T, 0> {
+	using type = typename std::vector<T>;
+};
+
+template <typename T, size_t N>
+auto make_vector_impl(const std::vector<std::size_t>& ls, T init_value) {
+	if constexpr(N == 0) {
+		return std::vector<T>(ls[N], init_value);
+	} else {
+		return typename make_vector_type<T, N>::type(
+			ls[N], make_vector_impl<T, (N - 1)>(ls, init_value));
+	}
+}
+
+template <typename T, std::size_t N>
+auto make_vector(const std::size_t (&ls)[N], T init_value) {
+	std::vector<std::size_t> dimensions(N);
+	for(int i = 0; i < N; i++) {
+		dimensions[N - i - 1] = ls[i];
+	}
+	return make_vector_impl<T, N - 1>(dimensions, init_value);
+}
+
+template <typename T>
+std::vector<T> make_vector(std::size_t size, T init_value) {
+	return std::vector<T>(size, init_value);
+}
+
+template <typename T>
+constexpr int sign(T x) {
+	return x < 0 ? -1 : x > 0 ? 1 : 0;
+}
+
+template <>
+constexpr int sign(double x) {
+	return x < -EPS ? -1 : x > EPS ? 1 : 0;
+}
+
+template <typename T>
+constexpr bool chmax(T& m, T x) {
+	if(m >= x) {
+		return false;
+	}
+	m = x;
+	return true;
+}
+
+template <typename T>
+constexpr bool chmin(T& m, T x) {
+	if(m <= x) {
+		return false;
+	}
+	m = x;
+	return true;
+}
+
+template <typename T>
+constexpr T square(T x) {
+	return x * x;
+}
+
+template <typename T>
+constexpr T pow(T a, int n) {
+	T ret = 1;
+	while(n != 0) {
+		if(n % 2) {
+			ret *= a;
+		}
+		a *= a;
+		n /= 2;
+	}
+	return ret;
+}
+
+template <typename T>
+constexpr T div_ceil(T a, T b) {
+	assert(b != 0);
+	if(a < 0 && b < 0) {
+		a = -a;
+		b = -b;
+	}
+	if(a >= 0 && b > 0) {
+		return (a + b - 1) / b;
+	}
+	return a / b;
+}
+
+template <typename T>
+constexpr T div_floor(T a, T b) {
+	assert(b != 0);
+	if(a < 0 && b < 0) {
+		a = -a;
+		b = -b;
+	}
+	if(a >= 0 && b > 0) {
+		return a / b;
+	}
+	assert(false);
+}
+
+template <typename T>
+constexpr bool is_power_of_two(T n) {
+	if constexpr(n == std::numeric_limits<T>::min()) {
+		return true;
+	}
+	return (n & (n - 1)) == 0;
+}
+
+constexpr std::size_t next_power_of_two(std::size_t n) {
+	if((n & (n - 1)) == 0) {
+		return n;
+	}
+	std::size_t ret = 1;
+	while(n != 0) {
+		ret <<= 1;
+		n >>= 1;
+	}
+	return ret;
+}
+
+template <typename T>
+constexpr T next_multiple_of(T a, T b) {
+	return div_ceil(a, b) * b;
+}
+
+template <typename T>
+constexpr bool is_mul_overflow(T a, T b) {
+	if(a >= 0 && b >= 0) {
+		return a > std::numeric_limits<T>::max() / b;
+	}
+	if(a <= 0 && b < 0) {
+		return a < div_ceil(std::numeric_limits<T>::max(), b);
+	}
+	if(a < 0) {
+		return a > std::numeric_limits<T>::min() / b;
+	}
+	if(b < 0) {
+		return a < div_ceil(std::numeric_limits<T>::max(), b);
+	}
+}
+
+template <typename T>
+constexpr T diff(T a, T b) {
+	return max(a, b) - min(a, b);
+}
+
+template <long long MOD = 1000000007>
+class ModInt {
+	public:
+	long long n;
+
+	constexpr ModInt() : n(0) {}
+	constexpr ModInt(long long n)
+		: n(n < 0	   ? n + MOD
+			: n >= MOD ? n - MOD
+					   : n) {}
+
+	constexpr long long get() const { return this->n; }
+	constexpr long long get_mod() const { return MOD; }
+
+	constexpr ModInt& operator=(const long long rhs) {
+		return *this = ModInt(rhs);
+	}
+	constexpr ModInt& operator+=(const ModInt rhs) {
+		return *this = ModInt(this->n + rhs.n);
+	}
+	constexpr ModInt& operator-=(const ModInt rhs) {
+		return *this = ModInt(this->n - rhs.n);
+	}
+	constexpr bool operator==(const ModInt rhs) const {
+		return this->n == rhs.n;
+	}
+};
+
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator+(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) += rhs;
+}
+
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs,
+								const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const ModInt<MOD>& lhs, const long long& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+template <long long MOD>
+constexpr ModInt<MOD> operator-(const long long& lhs, const ModInt<MOD>& rhs) {
+	return ModInt<MOD>(lhs) -= rhs;
+}
+
+template <long long MOD>
+std::ostream& operator<<(std::ostream& os, const ModInt<MOD>& x) {
+	return os << x.n;
+}
+
+template <long long MOD>
+std::istream& operator>>(std::istream& is, ModInt<MOD>& x) {
+	return is >> x.n;
+}
+
+/**
+ *  _       _                     _        ____
+ * (_)_ __ | |_   _ __ ___   __ _(_)_ __  / /\ \ _
+ * | | '_ \| __| | '_ ` _ \ / _` | | '_ \| |  | (_)
+ * | | | | | |_  | | | | | | (_| | | | | | |  | |_
+ * |_|_| |_|\__| |_| |_| |_|\__,_|_|_| |_| |  | ( )
+ *                                        \_\/_/|/
+ */
+
+using mint = ModInt<998244353>;
+
+const int N_MAX = 300;
+const int M_MAX = 100000;
+
+int n, m;
+ll a, b, c, d, e, f;
+ll x[M_MAX], y[M_MAX];
+
+unordered_map<ll, unordered_set<ll>> disabled;
+int used[N_MAX + 1][N_MAX + 1][N_MAX + 1];
+mint memo[N_MAX + 1][N_MAX + 1][N_MAX + 1];
+
+mint dp(int ab, int cd, int ef) {
+	int& is_used = used[ab][cd][ef];
+	mint& ret = memo[ab][cd][ef];
+	if(is_used != 0) {
+		return ret;
+	}
+	is_used = 1;
+
+	ll xx = a * ab + c * cd + e * ef;
+	ll yy = b * ab + d * cd + f * ef;
+
+	if(disabled[xx].count(yy) != 0) {
+		return 0;
+	}
+
+	if(ab + cd + ef == n) {
+		return ret = 1;
+	}
+
+	ret += dp(ab, cd, ef + 1);
+	ret += dp(ab, cd + 1, ef);
+	ret += dp(ab + 1, cd, ef);
+	return ret;
+}
+
+int main() {
+	cin >> n >> m;
+	cin >> a >> b >> c >> d >> e >> f;
+	REP(i, m) {
+		cin >> x[i] >> y[i];
+	}
+
+	REP(i, m) {
+		disabled[x[i]].insert(y[i]);
+	}
+
+	mint result = dp(0, 0, 0);
+
+	cout << result << endl;
+	return 0;
+}
