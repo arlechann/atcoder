5731ab2dfe7deb8f7931ec50e8e6d89046f5447f
arlechann <dragnov3728@gmail.com>
2023-11-07 18:44:55 +0900

    Update template.lisp

--- library/template.lisp
+++ library/template.lisp
@@ -1,165 +1,170 @@
-(unless (member :child-sbcl *features*)
-  #-swank (quit
-            :unix-status
-            (process-exit-code
-              (run-program *runtime-pathname*
-                           `("--control-stack-size" "128MB"
-                             "--noinform"
-                             "--disable-ldb"
-                             "--lose-on-corruption"
-                             "--end-runtime-options"
-                             "--eval" "(push :child-sbcl *features*)"
-                             "--script" ,(namestring *load-pathname*))
-                           :output t :error t :input t))))
+(in-package :cl-user)
 
-(eval-when (:compile-toplevel)
-  #-swank (proclaim '(optimize (speed 3) (debug 0) (safety 0))))
+;;;
+;;; utility
+;;;
+(defpackage :utility
+  (:use :cl)
+  (:export :nlet
+           :aif
+           :it
+           :dvector
+           :delay
+           :force
+           :symbol-intern
+           ))
+(in-package :utility)
+
+(defmacro nlet (name binds &body body)
+  `(labels ((,name ,(mapcar #'car binds) ,@body))
+     (,name ,@(mapcar #'cadr binds))))
+
+(defmacro aif (test then &optional else)
+  `(let ((it ,test))
+     (if it ,then ,else)))
+
+(defun dvector (&rest contents)
+  (make-array (length contents)
+              :initial-contents contents
+              :adjustable t
+              :fill-pointer t))
+
+(defstruct promise (value nil) thunk)
+
+(defmacro delay (expr)
+  `(make-promise :thunk (lambda () ,expr)))
+
+(defun force (ps)
+  (when (promise-thunk ps)
+    (setf (promise-value ps) (funcall (promise-thunk ps))
+          (promise-thunk ps) nil))
+  (promise-value ps))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (labels ((array-reference-reader (stream c n)
-             "Reader macro #&(array index) => (aref array index)"
-             (declare (ignore c n))
-             (let ((list (read stream)))
-               `(aref ,(car list) ,@(cdr list)))))
-    (setf *readtable* (copy-readtable))
-    (set-dispatch-macro-character #\# #\& #'array-reference-reader)))
+  (defun symbol-intern (sym &optional (package *package*))
+    (values (intern (symbol-name sym) package))))
 
-(defconstant mod-number 1000000007)
+;;;
+;;; input
+;;;
+(defpackage :input
+  (:use :cl
+        :utility)
+  (:export :input*
+           :def-input-reader))
+(in-package :input)
 
-(defmacro let-if (var cond tbody &optional fbody)
-  `(let ((,var ,cond))
-     (if ,var
-         ,tbody
-         ,fbody)))
+; (input* ((n fixnum)
+;          (v (vector (list fixnum1 fixnum1 n))))
+;   (list n v))
+; 3
+; 1 2
+; 3 4
+; 5 6
+; => (3 #((0 1) (2 3) (4 5)))
 
-(defmacro named-let (name binds &body body)
-  (let ((params (mapcar #'car binds))
-        (args (mapcar #'cadr binds)))
-    `(labels ((,name (,@params)
-                ,@body))
-       (,name ,@args))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *input-reader-table* (make-hash-table :test #'eq))
 
-(defun unfold (p f g seed &optional (tail-gen (lambda () '())))
-  (if (p seed)
-      (tail-gen seed)
-      (cons (f seed)
-            (unfold p f g (g seed) tail-gen))))
+  (defun set-input-reader (marker reader)
+    (setf (gethash marker *input-reader-table*) reader))
 
-(defun iota (c &optional (s 0) (step 1) (acc nil))
-  (if (zerop c)
-      (nreverse acc)
-      (iota (1- c) (+ s step) step (cons s acc))))
+  (defun get-input-reader (marker)
+    (gethash marker *input-reader-table*))
 
-(defun sum (list &optional (init 0))
-  (reduce #'+ list :initial-value init))
+  (defun input-typespec-marker (typespec)
+    (symbol-intern (if (listp typespec)
+                       (car typespec)
+                       typespec)
+                   :input))
 
-(defun chomp (str)
-  (string-right-trim '(#\Return #\Linefeed) str))
+  (defun input-typespec-reader (typespec)
+    (format t "typespec: ~S~%" typespec)
+    (let ((marker (input-typespec-marker typespec)))
+      (if (null marker)
+          nil
+          (funcall (get-input-reader marker) typespec))))
 
-(defun diff (a b)
-  (abs (- a b)))
+  (defun input-expand (forms)
+    (if (null forms)
+        nil
+        (mapcar (lambda (form)
+                  (list (car form)
+                        (input-typespec-reader (cadr form))))
+                forms)))
+  )
 
-(defun read-integer (&optional (in *standard-input*))
-  (declare (inline read-byte))
-  (labels ((number-char-p (b)
-             (<= #.(char-code #\0) b #.(char-code #\9)))
-           (minus-char-p (b)
-             (= b #.(char-code #\-)))
-           (to-number (b)
-             (- b #.(char-code #\0))))
-    (declare (inline number-char-p minus-char-p to-number))
-    (macrolet ((%read-byte ()
-                 '(the (unsigned-byte 8)
-                       #+swank (char-code (read-char in nil #\Nul))
-                       #-swank (read-byte in nil 0))))
-      (let* ((minus-p nil)
-             (x (loop for b = (%read-byte)
-                      if (number-char-p b)
-                        return (to-number b)
-                        end
-                      if (minus-char-p b)
-                        do (setf minus-p t))))
-        (declare (boolean minus-p) (fixnum x))
-        (the fixnum (loop for b = (%read-byte)
-                          and y = x then (+ (* y 10) (to-number b))
-                          unless (number-char-p b)
-                            return (funcall (if minus-p #'- #'+) y)))))))
 
-(defun split (x str &optional (acc nil))
-  (let ((pos (search x str))
-        (size (length x)))
-    (if pos
-        (split x (subseq str (+ pos size)) (cons (subseq str 0 pos) acc))
-        (nreverse (cons str acc)))))
+(defmacro input* (forms &body body)
+  `(let* ,(input-expand forms) ,@body))
 
-(defmacro minf (field &rest x)
-  `(setf ,field (min ,field ,@x)))
+(defmacro def-input-reader (marker params &body body)
+  `(progn (set-input-reader
+           ',marker
+           (macrolet ((reader (typespec)
+                        `(input-typespec-reader ,typespec)))
+             (lambda (,@params &optional arg)
+               (declare (ignore arg))
+               ,@body)))
+          ',marker))
 
-(defmacro maxf (field &rest x)
-  `(setf ,field (max ,field ,@x)))
+(eval-when (:compile-toplevel :execute)
+  (def-input-reader fixnum ()
+    '(read))
 
-(defmacro debug-print (x)
-  `(let ((y ,x))
-    (format t "~A: ~A~%" ',x y)
-    y))
+  (def-input-reader fixnum1 ()
+    '(1- (read)))
 
-(eval-when (:compile-toplevel)
-  (defun definput-helper-integer (bind)
-    (let ((var (second bind)))
-      `(defparameter ,var (read))))
-  
-  (defun definput-helper-string (bind))
-  (defun set-read-to-array (type array dimensions)
-    (let ((input (case (second type)
-                   ('integer #'read)
-                   ('string #'read-line)))
-          (size (array-total-size array)))
-      (dotimes (i size)
-        (setf (row-major-aref array i) (funcall input)))))
-  
-  (defun definput-helper-array (bind)
-    (destructuring-bind (type var dimensions) bind
-      (if (atom dimensions)
-          (setf dimensions (list dimensions)))
-      `(prog1
-         (defparameter ,var (make-array (mapcar #'symbol-value ',dimensions)))
-         (set-read-to-array ',type ,var ',dimensions))))
-  
-  (defun definput-helper-list (bind)
-    (let ((type (second (first bind)))
-          (var (second bind))
-          (size (third bind)))
-      `(defparameter ,var
-                     (named-let rec ((i ,size)
-                                     (acc nil))
-                       (if (zerop i)
-                           (nreverse acc)
-                           (rec (1- i)
-                                (cons (read) acc)))))))
-  
-  (defun definput-helper (bind)
-    (let ((type (first bind)))
-      (funcall (case type
-                 ('integer #'definput-helper-integer)
-                 ('string #'definput-helper-string)
-                 (otherwise (case (car type)
-                              ('array #'definput-helper-array)
-                              ('list #'definput-helper-list))))
-               bind))))
+  (def-input-reader double ()
+    '(let ((*read-default-float-format* 'double-float))
+      (read)))
 
-(defmacro definput (binds)
-  (cons 'progn (mapcar #'definput-helper binds)))
+  (def-input-reader list (typespec)
+    (let ((elems (cdr typespec)))
+      (if (null elems)
+          nil
+          `(cons ,(reader (car elems))
+                 ,(reader (cons 'list (cdr elems)))))))
 
-(defmacro defdp (name args &body body)
-  (let ((argc (length args))
-        (key (gensym)))
-    `(let ((dp (make-hash-table :test #'equal)))
-       (defun ,name ,args
-         (let ((,key ,@(if (= argc 1) args `((list ,@args)))))
-           (let-if memo (gethash ,key dp)
-                   memo
-                   (setf (gethash ,key dp)
-                         (progn ,@body))))))))
+  (def-input-reader vector (typespec)
+    (let ((vec (gensym "VEC"))
+          (index (gensym "INDEX"))
+          (elem (cadr typespec))
+          (len (caddr typespec)))
+      `(let ((,vec (make-array ,len)))
+         (dotimes (,index ,len ,vec)
+           (setf (aref ,vec ,index) ,(reader elem))))))
+  )
 
-;;; ここまで
+;;;
+;;; algorithm
+;;;
+(defpackage :algorithm
+  (:nicknames :algo)
+  (:use :cl)
+  (:export :meguru-method
+           ))
 
+(defun meguru-method (ok ng pred)
+  (if (<= (abs (- ok ng)) 1)
+      ok
+      (let ((mid (floor (+ ok ng) 2)))
+        (if (funcall pred mid)
+            (meguru-method mid ng pred)
+            (meguru-method ok mid pred)))))
+
+;;;
+;;; atcoder
+;;;
+(defpackage :atcoder
+  (:nicknames :ac)
+  (:use :cl
+        :utility
+        :input))
+(in-package :atcoder)
+
+(defun main ()
+  (input* ((n (list fixnum fixnum)))
+    (format t "~A~%" n)))
+
+(main)
