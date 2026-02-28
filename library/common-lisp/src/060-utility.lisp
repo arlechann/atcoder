c5a33820100d988add5105b002f2942e24226df1
arlechann <dragnov3728@gmail.com>
2026-02-28 20:42:36 +0900

    Fix utility.lisp

--- library/common-lisp/src/060-utility.lisp
+++ library/common-lisp/src/060-utility.lisp
@@ -1,18 +1,15 @@
 ;;; utility
 ;;;
-(defpackage utility
-  (:use :cl
-        :utility.syntax
-        :utility.number
-        :utility.base
-        :utility.window))
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (dolist (src '(:utility.syntax
-                 :utility.number
-                 :utility.base
-                 :utility.window))
-    (do-external-symbols (sym src)
-      (export sym :utility))))
+  (let* ((sources '(:utility.syntax
+                    :utility.number
+                    :utility.base
+                    :utility.window))
+         (pkg (or (find-package :utility)
+                  (make-package :utility :use '(:cl)))))
+    (dolist (src sources)
+      (use-package src pkg)
+      (do-external-symbols (sym src)
+        (export sym pkg)))))
 
 ;;;
